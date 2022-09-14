3f8f53bee5742a448a45e9c3789bba1c7fd12197
webhero-nur <nurealom522@gmail..com>
2022-09-14 18:55:23 +0600

    Learned Multi Parameter Function

---
+++ parameter-1.js
@@ -0,0 +1,22 @@
+// function add(num1, num2) {
+//     console.log('going to add:', num1, num2);
+// }
+
+// var number1 = 10;
+// var number2 = 20;
+// add();
+// add(number1);
+// add(number1, number2);
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log('result:', sum);
+}
+
+sum(14);
+sum(14, 10);
+sum(14, 10, 40);
+sum(14, 10, 40, 20);
+sum(14, 10, 40, 20, 11);
+sum(14, 10, 40, 20, 11, 12);
